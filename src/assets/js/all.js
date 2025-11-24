2646e75cee840232902e8671c0741ed43df4f45c
Melia Burnley <melia.burnley@justice.gov.uk>
2025-11-24 14:59:42 +0000

    CDPS-1674: Add webcam capture component

--- src/assets/js/all.js
+++ src/assets/js/all.js
@@ -1,5 +1,7 @@
 import * as addressAutosuggest from '../dps/components/address-autosuggest'
+import * as webcamCapture from '../dps/components/webcam-capture'
 
 export function initAll() {
   addressAutosuggest.init()
+  webcamCapture.init()
 }
