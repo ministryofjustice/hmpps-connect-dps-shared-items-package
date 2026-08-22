8882d865af0efc036458c71295c4e2732b6165da
Igor <igor.ushkarev1@justice.gov.uk>
2026-08-22 16:52:24 +0100

    Build css with rollup to ensure that components’ scss files can compile in client apps successfully

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,6 +2,8 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
+    // Needed by rollup for watching files
+    'node_modules/@parcel/watcher@2.6.0': 'ALLOW',
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
